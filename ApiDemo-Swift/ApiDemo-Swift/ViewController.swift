55a1afc1700b3100af8c78a9e6365a895f32509d
suzp1984 <suzp1984@gmail.com>
2015-11-23 16:01:05 +0800

    init project

---
+++ ApiDemo-Swift/ApiDemo-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ApiDemo-Swift
+//
+//  Created by Moses Su on 11/23/15.
+//  Copyright © 2015 iboxpay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
