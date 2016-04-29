bc62dc1cd4ab65df7bf8337dd4395a7d0aaeea6c
suzp1984 <suzp1984@gmail.com>
2016-04-29 23:20:44 +0800

    remove main.storyboard

--- ApiDemo-Swift/ApiDemo-Swift/AppDelegate.swift
+++ ApiDemo-Swift/ApiDemo-Swift/AppDelegate.swift
@@ -14,8 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow()
+        self.window!.rootViewController = UIViewController()
+        self.window!.backgroundColor = UIColor.darkGrayColor()
+        self.window!.makeKeyAndVisible()
+        
         return true
     }
 
