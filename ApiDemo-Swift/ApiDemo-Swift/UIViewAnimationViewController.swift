03102dc54d3130a76c70f0c7f28465e41851bea7
suzp1984 <suzp1984@gmail.com>
2016-07-01 11:54:08 +0800

    add more UIView animations

--- ApiDemo-Swift/ApiDemo-Swift/UIViewAnimationViewController.swift
+++ ApiDemo-Swift/ApiDemo-Swift/UIViewAnimationViewController.swift
@@ -43,6 +43,46 @@ class UIViewAnimationViewController: UIViewController {
             v2.backgroundColor = UIColor.cyanColor()
             v2.center.x += 100
         })
+        
+        let v3 = UIView()
+        let v4 = UIView()
+        self.view.addSubview(v3)
+        self.view.addSubview(v4)
+        v3.backgroundColor = UIColor.brownColor()
+        v4.backgroundColor = UIColor.blackColor()
+        
+        v3.translatesAutoresizingMaskIntoConstraints = false
+        v4.translatesAutoresizingMaskIntoConstraints = false
+        v3.widthAnchor.constraintEqualToConstant(100).active = true
+        v3.heightAnchor.constraintEqualToConstant(100).active = true
+        v4.widthAnchor.constraintEqualToAnchor(v3.widthAnchor).active = true
+        v4.heightAnchor.constraintEqualToAnchor(v3.heightAnchor).active = true
+        
+        v3.topAnchor.constraintEqualToAnchor(v2.bottomAnchor).active = true
+        v3.leftAnchor.constraintEqualToAnchor(self.view.leftAnchor).active = true
+        v4.topAnchor.constraintEqualToAnchor(v3.topAnchor).active = true
+        v4.leftAnchor.constraintEqualToAnchor(v3.leftAnchor).active = true
+        v3.alpha = 1
+        v4.alpha = 0
+        UIView.animateWithDuration(2, animations: {
+            v4.alpha = 1
+            v3.alpha = 0
+        })
+        
+        let v5 = UIView()
+        v5.backgroundColor = UIColor.darkGrayColor()
+        v5.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(v5)
+        v5.widthAnchor.constraintEqualToConstant(100).active = true
+        v5.heightAnchor.constraintEqualToConstant(100).active = true
+        v5.topAnchor.constraintEqualToAnchor(v3.bottomAnchor).active = true
+        v5.leftAnchor.constraintEqualToAnchor(self.view.leftAnchor).active = true
+        UIView.animateWithDuration(2, animations: {
+            v5.backgroundColor = UIColor.greenColor()
+            UIView.performWithoutAnimation{
+                v5.center.y += 100
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
