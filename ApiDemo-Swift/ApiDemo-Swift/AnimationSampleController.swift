9f97ab323f8dba347aff0eedfa1352a85521009d
suzp1984 <suzp1984@gmail.com>
2016-07-22 12:23:50 +0800

    add UIDynamics Animation

--- ApiDemo-Swift/ApiDemo-Swift/AnimationSampleController.swift
+++ ApiDemo-Swift/ApiDemo-Swift/AnimationSampleController.swift
@@ -14,7 +14,7 @@ class AnimationSampleController: UIViewController, UINavigationControllerDelegat
     
     let cellIdentifier = "animation"
     let demos = ["ImageView Animation", "UIView Animation", "UIView Animation 2", "UIView Transition", "Layer Animation", "Grouped Animation", "Frozen Animation", "Layer Transition", "Actions",
-                 "Emitter Layer", "CIFilter Transition"]
+                 "Emitter Layer", "CIFilter Transition", "Dynamics View"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -88,6 +88,8 @@ class AnimationSampleController: UIViewController, UINavigationControllerDelegat
             self.navigationController!.pushViewController(EmitterLayerViewController(), animated: true)
         case demos[10]:
             self.navigationController!.pushViewController(CIFilterTransitionViewController(), animated: true)
+        case demos[11]:
+            self.navigationController!.pushViewController(DynamicsViewController(), animated: true)
         default:
             break
         }
